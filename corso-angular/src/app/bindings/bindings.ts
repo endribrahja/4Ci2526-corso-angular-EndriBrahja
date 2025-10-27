a1cdb8c291d4490e151c3dd950740437f6f973f1
endribrahja <endri.brahja@marconirovereto.it>
2025-10-27 11:05:26 +0000

    lezione 27/10/2025

--- corso-angular/src/app/bindings/bindings.ts
+++ corso-angular/src/app/bindings/bindings.ts
@@ -10,7 +10,21 @@ export class Bindings {
   // crea un signal se si aggiorna alla modifica
   studente: WritableSignal<string> = signal("Federico");
 
+  // signal per un contatore numerico
+  contatore: WritableSignal<number> = signal(0);
+
+  immagini: string[] = ['/bici.jpg', '/motorino.jpg', '/macchina.jpg', '/aereo.jpg'];
+  indiceImmagine: WritableSignal<number> = signal(0);
+
   cambiaNome(): void {
     this.studente.set("Luca Boschi");
+  }
+
+  incrementa(): void {
+    this.contatore.update(n => n + 1);
+  }
+
+  decrementa(): void {
+    this.contatore.update(n => n - 1);
   }
 }
